04e3f5b01cbce33d1b341909964f4af031b70c2b
pueri <pyro@outlook.at>
2019-02-07 15:52:02 +0100

    Handle multiple delimiters in calculator add method

--- src/calculator.spec.ts
+++ src/calculator.spec.ts
@@ -7,29 +7,43 @@ describe('Calculator', () => {
     calculator = new Calculator();
   });
 
-  test('empty string returns 0', () => {
-    expect(calculator.add('')).toEqual(0);
-  });
+  describe('add', () => {
+    test('empty string returns 0', () => {
+      expect(calculator.add('')).toEqual(0);
+    });
 
-  test('sums up numbers of string', () => {
-    expect(calculator.add('2,4,6,8')).toEqual(20);
-  });
+    test('sums up numbers of string', () => {
+      expect(calculator.add('2,4,6,8')).toEqual(20);
+    });
 
-  test('allows new line delimitter', () => {
-    expect(calculator.add('2\n4,6,8')).toEqual(20);
-  });
+    test('allows new line delimitter', () => {
+      expect(calculator.add('2\n4,6,8')).toEqual(20);
+    });
 
-  test('ignores numbers greater than 1000', () => {
-    expect(calculator.add('2,4,6,8,1001,2002')).toEqual(20);
-  });
+    test('ignores numbers greater than 1000', () => {
+      expect(calculator.add('2,4,6,8,1001,2002')).toEqual(20);
+    });
 
-  test('throws on negative numbers', () => {
-    expect(() => calculator.add('2,-4,3,-5')).toThrowError(
-      'Negatives not allowed: -4,-5'
-    );
-  });
+    test('throws on negative numbers', () => {
+      expect(() => calculator.add('2,-4,3,-5')).toThrowError(
+        'Negatives not allowed: -4,-5'
+      );
+    });
 
-  test('handles passed delimiters', () => {
-    expect(calculator.add('//;\n1;2')).toEqual(3);
+    test('handles passed delimiters', () => {
+      expect(calculator.add('//;\n1;2')).toEqual(3);
+    });
+
+    test('handles delimiters of any length', () => {
+      expect(calculator.add('//[|||]\n1|||2|||3')).toEqual(6);
+    });
+
+    test('handles multiple delimiters', () => {
+      expect(calculator.add('//[|][%]\n1|2%3')).toEqual(6);
+    });
+
+    test('handles multiple delimiters of any length', () => {
+      expect(calculator.add('//[|||][%%%]\n1|||2%%%3')).toEqual(6);
+    });
   });
 });
--- src/calculator.ts
+++ src/calculator.ts
@@ -1,19 +1,39 @@
+import { escapeRegExp } from 'lodash';
+
 export class Calculator {
   public add(data: string): number {
     const [first, rest] = data.split(/\n/, 2);
-    const del = /^\/\//.test(first) ? new RegExp(first.substr(2)) : /(,|\n)/;
+
+    let delimiter = /(,|\n)/;
+
+    if (/^\/\//.test(first)) {
+      delimiter = new RegExp(
+        /^\/\/\[.+\]$/.test(first)
+          ? first
+              .substring(3, first.length - 1)
+              .split('][')
+              .map(x => escapeRegExp(x))
+              .join('|')
+          : first.substr(2)
+      );
+    }
+
     const array = /^\/\//.test(first) ? rest : data;
     const numbers = array
-      .split(del)
+      .split(delimiter)
       .map(x => Number(x))
       .filter(x => typeof x === 'number' && x <= 1000);
 
+    this.interceptNegatives(numbers);
+
+    return numbers.length > 0 ? numbers.reduce((a, b) => a + b) : 0;
+  }
+
+  private interceptNegatives(numbers: number[]): void {
     const negatives = numbers.filter(x => x < 0);
 
     if (negatives.length > 0) {
       throw new Error(`Negatives not allowed: ${negatives.join(',')}`);
     }
-
-    return numbers.reduce((a, b) => a + b);
   }
 }
